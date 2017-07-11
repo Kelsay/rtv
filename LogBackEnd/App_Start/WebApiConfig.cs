cffe378c1d7db06a3c2f68b755e4444600420e5a
Krzysztof Fijolek <fijolek.krzysztof@gmail.com>
2017-07-11 19:10:45 +0200

    BE Added CORS support

--- LogBackEnd/App_Start/WebApiConfig.cs
+++ LogBackEnd/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace LogBackEnd
 {
@@ -10,6 +11,11 @@ namespace LogBackEnd
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            // Enable CORS
+            // TODO - not the safest config ever. Just for the test purposes
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
